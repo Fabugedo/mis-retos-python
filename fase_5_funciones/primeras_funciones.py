b3d51d5553c08f1b17c0ef1efe194908c2a4edc6
Fabrizio Bugedo <fa.bugedo@duocuc.cl>
2026-09-14 21:13:34 -0300

    feat: prim funciones y modulo operaciones imp

---
+++ fase_5_funciones/primeras_funciones.py
@@ -0,0 +1,17 @@
+import operaciones
+
+
+def saludar(nombre):
+    return f"Hola {nombre}, Bienvenido a la fase 5!"
+
+
+
+mensaje = saludar("Fabrizio")
+
+print(mensaje)
+
+#####################################################################
+
+suma = operaciones.sumar(5,10)
+
+print(suma)
