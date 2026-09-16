07bc5c1ebba1d0177f168052665df4535bc780ce
Fabrizio Bugedo <fa.bugedo@duocuc.cl>
2026-09-15 21:25:29 -0300

    feat: funciones con parametros por defecto e importacion modular

--- fase_5_funciones/operaciones.py
+++ fase_5_funciones/operaciones.py
@@ -1,2 +1,14 @@
 def sumar(a, b):
     return a + b
+
+def saludar(nombre="invitado"):
+    return f"Hola {nombre}, Bienvenido a la fase 5!"
+
+def calcular_precio_total(precio_bbase, cantidad=1, impuesto=0.19):
+                    precio_total = precio_bbase * cantidad
+                    impuesto_altotal = precio_total * (1 + impuesto )
+                    return impuesto_altotal
+def aplicar_descuento(monto_total, porcentaje_descuento=0):
+        monto_final = monto_total * (1 - (porcentaje_descuento / 100))
+        return monto_final
+
--- fase_5_funciones/primeras_funciones.py
+++ fase_5_funciones/primeras_funciones.py
@@ -1,12 +1,11 @@
 import operaciones
 
 
-def saludar(nombre):
-    return f"Hola {nombre}, Bienvenido a la fase 5!"
 
 
 
-mensaje = saludar("Fabrizio")
+
+mensaje = operaciones.saludar()
 
 print(mensaje)
 
@@ -15,3 +14,14 @@ print(mensaje)
 suma = operaciones.sumar(5,10)
 
 print(suma)
+
+#######################################################################
+
+cliente_A = operaciones.calcular_precio_total(10000)
+cliente_B = operaciones.calcular_precio_total(5000, 3)
+cliente_c = operaciones.calcular_precio_total(20000, 2, 0.1)
+cliente_c_desc = operaciones.aplicar_descuento(cliente_c, 10)
+
+print(f"Total con cliente A : {cliente_A}")
+print(f"Total con cliente B: {cliente_B}")
+print(f"Total con cliente C: {cliente_c} pero aplicamos un descuento: {cliente_c_desc}")
